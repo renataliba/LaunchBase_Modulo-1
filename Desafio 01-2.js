ed8381bcad0092c54fcfe803d9ce0e6cc3371b0a
renataliba <42476676+renataliba@users.noreply.github.com>
2020-02-02 17:21:55 -0300

    Update Desafio 01-2.js

--- Desafio 01-2.js
+++ Desafio 01-2.js
@@ -18,4 +18,60 @@ const usuario = {
 console.log(`O ${usuario.nome} trabalha na empresa ${usuario.empresa.nome}, localizada na ${usuario.empresa.endereço}, n ${usuario.empresa.número}.`)
 console.log(`O usuário ${usuario.nome} tem ${usuario.idade} anos e usa a tecnologia ${usuario.tecnologias[1].nome} com especialidade em ${usuario.tecnologias[1].especialidade}`)
 
+_______
 
+//Minha versão
+
+        // Objetos - Abaixo a estrutura de Objetos
+// const aluno01 = {
+//     nome : "Renata",
+//     nota : 9.8
+// }
+// const aluno02 = {
+//     nome : "Maria",
+//     nota : 10
+// }
+// const media = (aluno01.nota + aluno02.nota)
+// console.log(media)
+
+
+        // Array é o mesmo que vetores. É uma variável que contém diversos
+// valores dentro dela.
+// a estrutura é assim:
+// const alunos = [ 
+//     {
+//         nome : "Renata",
+//         nota : 9.8
+//     },
+//     {
+//         nome : "Bia",
+//         nota : 10
+//     }
+// ]
+// const media = (alunos[0].nota + alunos[1].nota)/2
+// console.log(media)
+
+// const nomeDeAlunos = ["Jose","Maria","Ricardo"]
+// console.log(nomeDeAlunos)
+// console.log(alunos[1])
+
+
+const Empresa = {
+    nome: "Rocketseat",
+    cor: "roxo",
+    Foco: "Programação",
+    Endereco: {
+        Rua: "Rua Guilherme Gembala",
+        numero: 260
+    },
+}
+
+const usuario = [
+    {nome: `Renata`, idade: 42, Tecno: 'JavaScript'},
+    {nome: `Paulo`, idade: 43, Tecno: 'JavaScript'}
+]
+
+console.log(`A empresa ${Empresa.nome} está localizada na ${Empresa.Endereco.Rua}, ${Empresa.Endereco.numero}`)
+console.log(`Os usuários e suas competências são:`)
+console.log(`${usuario[0].nome}, ${usuario[0].idade}, ${usuario[0].Tecno} e 
+${usuario[1].nome}, ${usuario[1].idade}, ${usuario[1].Tecno}`)
